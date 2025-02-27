c9bc95aac87764eeaae0a3473d30b622c76909f2
Piyush Kumar <piyushkumar1116@gmail.com>
2025-02-27 22:55:30 +0530

    add Swagger support and update app service greeting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const Package = require('../package.json');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+  });
+
+  const port = Number(process.env.PORT) || 3006;
+  const host = '0.0.0.0';
+
+  const config = new DocumentBuilder()
+    .setTitle('Analytics API')
+    .setDescription('The Analytics API description')
+    .setVersion('1.0')
+    .addTag('analytics')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port, host, async () => {
+    console.info('Server Started', {
+      environment: process.env.NODE_ENV,
+      version: Package.version,
+      port: port,
+      host: host,
+      process_id: process.pid,
+      docs:
+        process.env.NODE_ENV !== 'production'
+          ? `http://localhost:${port}/api#/`
+          : 'Not Available!',
+    });
+  });
 }
+
 bootstrap();
