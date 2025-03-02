4f2e5ff89033389bdc2e6209cd4bd911cbbf7416
Piyush Kumar <piyushkumar1116@gmail.com>
2025-03-02 13:11:48 +0530

    feat: add AppController and AppService to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { AnalyticsModule } from './analytics/analytics.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [CoreModule, AuthModule, AnalyticsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
